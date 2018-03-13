8bd686d589d73c14c5684a59f77e1782d3bfa65a
Kathryn Elizabeth Taylor <kathryn.taylor@stu.jefferson.kyschools.us>
2018-03-13 09:40:09 -0400

    First Commit

---
+++ KathrynTaylor.py
@@ -0,0 +1,19 @@
+"""
+This program is about finding your type of genre and favorite movie.
+Just answer the questions given and you willg et the answer.
+
+"""
+var = input("What kind of movie do you like?")
+if var == "action":
+    input("Which action movie?")
+elif var == "horror":
+    input("Which horror movie?")
+elif var == "si-fi?":
+    input("Which si-fi movie?")
+
+new_list = ["The Avengers", "The Terminator", "Wonder Woman"]
+Output = "Wonder Woman"
+print(new_list[2])
+
+Output = "The Avengers"
+print(new_list[0])
