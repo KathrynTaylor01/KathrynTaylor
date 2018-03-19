8c21ba83651da2fa1f2077f2fd4e4d5ba745b708
Kathryn Elizabeth Taylor <kathryn.taylor@stu.jefferson.kyschools.us>
2018-03-19 09:30:41 -0400

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+computer_terms = {
+    "Bit": ("a binary digit."),
+    "Computer": ("an electronic device for storing and processing data in a device, but giving it instructions."),
+    "Cookie": ("a packet of info that travels around web servers."),
+    "Ethernet": ("a computer network technology that describes data flowing in to the network."),
+    "Trojan Horse": (" a virus disguised as a legitinmate software."),
+    "Zip": ("a file format to compress files."),
+    }
